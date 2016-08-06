962770984695b59a65f777ebdcec5d59281499ec
Michiaki <kanadebito63@yahoo.co.jp>
2016-08-06 03:12:50 +0000

    add /hi

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
   "Hello,World!"
+end
+
+get "/hi" do
+  "hi with your name"
+end
+
+post "/hi" do
+  j = JSON.parse(request.body.string)
+  j["events"].map{ |e|
+    if e["message"]
+      "Hi, #{e["message"]["nickname"]}!"
+    end
+  }.compact.join("\n")+"\n"
 end
